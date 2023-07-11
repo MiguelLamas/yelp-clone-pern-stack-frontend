35bb037f2323dc61ab8260331741980dfb8e5c59
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-07-11 16:10:27 +0100

    Added full URL from Render api

--- src/apis/RestaurantFinder.js
+++ src/apis/RestaurantFinder.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-
-const baseURL = process.env.NODE_ENV === 'production' 
-? "api/v1/restaurants"
-: "http://localhost:5000/api/v1/restaurants";
-
-
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://yelp-clone-pern-stack-api.onrender.com/api/v1/restaurants"
+    : "http://localhost:5000/api/v1/restaurants";
 
 export default axios.create({
-    baseURL: baseURL,
+  baseURL: baseURL,
 });

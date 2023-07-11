6c94b8883a68991f5b35c1c0581d420b38470222
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-07-11 16:20:18 +0100

    Amends to baseURL

--- src/apis/RestaurantFinder.js
+++ src/apis/RestaurantFinder.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://yelp-clone-pern-stack-api.onrender.com/api/v1/restaurants"
+    ? "api/v1/restaurants"
     : "http://localhost:5000/api/v1/restaurants";
 
 export default axios.create({
